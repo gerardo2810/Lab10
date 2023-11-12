12d2b2ce939c5d2e2e09228d7770a7ae4148c3bd
gerardo2810 <144623841+gerardo2810@users.noreply.github.com>
2023-11-12 10:10:45 -0600

    lab10

--- src/main/java/com/example/webapphr1_2023/Beans/Location.java
+++ src/main/java/com/example/webapphr1_2023/Beans/Location.java
@@ -2,6 +2,11 @@ package com.example.webapphr1_2023.Beans;
 
 public class Location {
     private int locationId;
+    private String street_address;
+    private String postal_code;
+    private String city;
+    private String state_province;
+    //private Countries country_id;
 
     public int getLocationId() {
         return locationId;
